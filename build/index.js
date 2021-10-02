c01cbcf21346560f8b3edac17cf91e1ec54e711e
Vijay Palaskar <vijaypalaskar@rediffmail.com>
2021-10-02 13:22:09 +0530

    node js server setup

--- build/index.js
+++ build/index.js
@@ -1,23 +1,4 @@
 "use strict";
-var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
-}) : (function(o, m, k, k2) {
-    if (k2 === undefined) k2 = k;
-    o[k2] = m[k];
-}));
-var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
-    Object.defineProperty(o, "default", { enumerable: true, value: v });
-}) : function(o, v) {
-    o["default"] = v;
-});
-var __importStar = (this && this.__importStar) || function (mod) {
-    if (mod && mod.__esModule) return mod;
-    var result = {};
-    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
-    __setModuleDefault(result, mod);
-    return result;
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -25,17 +6,18 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const dotenv_1 = __importDefault(require("dotenv"));
 dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
-const bodyParser = __importStar(require("body-parser"));
 const cors_1 = __importDefault(require("cors"));
 const db_1 = require("./db");
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 const PORT = process.env.PORT;
 const Hotel_1 = __importDefault(require("./Hotel"));
 app.use((0, cors_1.default)());
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use(express_1.default.static(path_1.default.join('./dist/webapp/')));
 app.get('/', (req, res, next) => {
-    res.json({ message: 'hello from server!' });
+    res.sendFile(path_1.default.join('./dist/webapp/index.html'));
 });
 app.use('/api/hotel', Hotel_1.default);
 app.listen(PORT, () => {
--- src/app.js
+++ src/app.js
@@ -1,23 +1,24 @@
-import dotenv from 'dotenv';
-dotenv.config();
-import express from 'express';
-import cors from 'cors';
-import { connect } from './db';
-import path from 'path';
+var express = require('express');
+var path = require('path');
+var cors = require('cors');
 const app = express();
-const PORT = process.env.PORT;
-import routes from './Hotel';
+const PORT = 5000;
+
 app.use(cors());
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
+
+// serve angular front end files from root path
 app.use(express.static('dist/webapp'));
-app.get('/*', (req, res, next) => {
+
+// rewrite virtual urls to angular app to enable refreshing of internal pages
+app.get('*', function (req, res, next) {
     res.sendFile(path.resolve('dist/webapp/index.html'));
 });
-
-app.use('/api/hotel', routes);
 
 app.listen(PORT, () => {
     // connect();
     console.log(`server listening on PORT: ${PORT}`);
 });
+
+
